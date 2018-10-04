6a950fcb3e2601a1adafa1364059d66c907231df
esadeli <efrat.sadeli@gmail.com>
2018-10-05 00:54:11 +0700

    Get Recipes Category

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -6,7 +6,21 @@ class RecipeController {
 
     // get category
     static getCategory(req,res){
-        
+        axios({
+            method: 'GET',
+            url: 'https://www.themealdb.com/api/json/v1/1/categories.php'
+        })
+          .then(results=>{
+              res.status(200).json({
+                  msg: 'List of categories',
+                  data: results.data
+              })
+          })
+          .catch(error =>{
+              res.status(500).json({
+                  msg: 'ERROR: ',error
+              })
+          })
     }
 }
 
--- routes/recipeRoute.js
+++ routes/recipeRoute.js
@@ -4,6 +4,6 @@ const router = require('express').Router()
 const RecipeController = require('../controllers/recipeController')
 
 // get category
-
+router.get('/category', RecipeController.getCategory)
 
 module.exports = router

ec94e4aabd87276747378cb574d34d3c008d3ab3
gandryeanb <gstandrianb@gmail.com>
2018-10-05 15:11:48 +0700

    fix bugs

--- routes/signUpRoute.js
+++ routes/signUpRoute.js
@@ -1,7 +1,7 @@
 const route = require('express').Router()
 const SignUpController = require('../controllers/signUpController')
-const isLogin = require('../middleware/isLogin')
+// const isLogin = require('../middleware/isLogin')
 
-route.post('/', isLogin, SignUpController.signUp)
+route.post('/', SignUpController.signUp)
 
 module.exports = route

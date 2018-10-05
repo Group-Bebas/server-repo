21efcf8c3c93e362bd13e560b1231669abe25afc
gandryeanb <gstandrianb@gmail.com>
2018-10-05 12:42:14 +0700

    fixing console

--- controllers/PlaceController.js
+++ controllers/PlaceController.js
@@ -3,28 +3,11 @@ const axios = require('axios')
 class PlaceController {
 
   static getNearbyPlace (req, res) {
-    let query = {
-      loc1: 'mall',
-      loc2: 'bar',
-      loc3: 'beauty_salon',
-      loc4: 'book_store',
-      loc5: 'cafe',
-      loc6: 'library',
-      loc7: 'movie_theater',
-      loc8: 'museum',
-      loc9: 'night_club',
-      loc10: 'park',
-      loc11: 'shopping_mall',
-      loc12: 'spa',
-      loc13: 'store',
-      loc14: 'supermarket',
-      loc15: 'zoo'
-    }
-    // let query = req.params.place
+    let query = req.params.place
     let lat = req.params.lat
     let lng = req.params.lng
     axios({
-      url: `https://maps.googleapis.com/maps/api/place/textsearch/json?location=${lat},${lng}&radius=1500&query=${query.loc1}&key=${process.env.API_KEY_GOOGLE3}`,
+      url: `https://maps.googleapis.com/maps/api/place/textsearch/json?location=${lat},${lng}&radius=1500&query=${query}&key=${process.env.API_KEY_GOOGLE3}`,
       method: 'get'
     })
       .then(data => {
